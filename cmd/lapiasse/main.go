2d88d56a68fdbe393d1d538f161f982910832725
Adeynack <Adeynack@users.noreply.github.com>
2025-12-13 15:58:47 +0100

    supper env LAPIASSE_DEFAULT_CMD

--- cmd/lapiasse/main.go
+++ cmd/lapiasse/main.go
@@ -3,6 +3,12 @@ package main
 import "os"
 
 func main() {
+	// If LAPIASSE_DEFAULT_CMD is set and no subcommand is provided, use the command from the env var.
+	// e.g.: LAPIASSE_DEFAULT_CMD=serve lapiasse is equivalent to lapiasse serve
+	if cmd := os.Getenv("LAPIASSE_DEFAULT_CMD"); cmd != "" && len(os.Args) == 1 {
+		os.Args = append(os.Args, cmd)
+	}
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
