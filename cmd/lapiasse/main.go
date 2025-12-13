dce53e450454767a459ccda2282ccefe3bc6dcf3
Adeynack <Adeynack@users.noreply.github.com>
2025-12-13 15:50:30 +0100

    Minimal TUI for test

--- cmd/lapiasse/main.go
+++ cmd/lapiasse/main.go
@@ -1,5 +1,10 @@
 package main
 
+import "os"
+
 func main() {
-	Execute()
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
--- pkg/tui/instance.go
+++ pkg/tui/instance.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 
+	"adeynack.net/lapiasse/pkg/api"
 	"adeynack.net/lapiasse/pkg/app"
 	"adeynack.net/lapiasse/pkg/applog"
+	"adeynack.net/lapiasse/pkg/platform/ctxval"
 )
 
 type Instance struct {
@@ -31,9 +33,26 @@ func (i *Instance) Close() error {
 }
 
 func (i *Instance) Run() error {
+	ctx := i.App.Context()
 	applog.Info(i.App.Context(), "Running TUI")
 
-	// Temporary "TUI" ;-)
+	// Placeholder TUI
+	repo := ctxval.MustResolve[api.StrictServerInterface](ctx)
+
+	booksResponse, err := repo.GetBooks(ctx, api.GetBooksRequestObject{})
+	if err != nil {
+		return fmt.Errorf("failed to get books: %w", err)
+	}
+	switch resp := booksResponse.(type) {
+	case api.GetBooks200JSONResponse:
+		fmt.Println("Books:")
+		for i, book := range resp.Books {
+			fmt.Printf("%d: %s\n", i+1, book.Name)
+		}
+	default:
+		return fmt.Errorf("unexpected response type: %T", booksResponse)
+	}
+
 	fmt.Println("Press ENTER to exit")
 	_, _ = fmt.Scanln()
 
